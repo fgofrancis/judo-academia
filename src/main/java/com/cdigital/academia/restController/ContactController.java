ba2ec46bbf19cd017dfd81715d7f1a10b8a8dd34
Tu nombre <Tu correo>
2026-04-07 18:14:18 -0400

    env2

--- src/main/java/com/cdigital/academia/restController/ContactController.java
+++ src/main/java/com/cdigital/academia/restController/ContactController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
 @RestController
 public class ContactController {
 
@@ -79,6 +81,7 @@ public class ContactController {
             return ResponseEntity.status(500).body("Error al enviar mensaje");
         }
     }
+
 
     @PostMapping("/contacto-html-tabla")
     public ResponseEntity<String> enviarFormularioHtmlTabla(
